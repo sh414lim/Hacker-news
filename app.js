43fa961228259a29a9a3d23ebdd720ea2ab3f8b1
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-11-03 22:26:55 +0900

    페이징

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const ajax = new XMLHttpRequest();
 const content=document.createElement('div')
 const NEWS_URL ='https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL='https://api.hnpwa.com/v0/item/@id.json';
+const store={
+    currentPage:1,
+}
+
 
 function getDate(url){ // ajax 함수
     ajax.open('GET' ,url,false) //동기적 처리
@@ -13,41 +17,73 @@ function getDate(url){ // ajax 함수
 }
 
 
-const newsFeed=getDate(NEWS_URL); 
-const ul =document.createElement('ul');
-
-window.addEventListener('hashchange',function(){
-        //loaction 브라우저가 제공해주는 객체 ->주소와 관련된 다양한 정보를 알 수 있다
-   const id = location.hash.substr(1);//#제거
-
-    const newsCotent=getDate(CONTENT_URL.replace('@id',id));
-    const title = document.createElement('h1');
-
-    container.innerHTML=`
-        <h1> ${newsCotent.title} <h1/>
-            <div>
-                <a href ="#">목록으로<a/>
-            <div/>
-    `;
-
-
-})
+function newsFead(){ //글 목록 함수
+    const newsFeed=getDate(NEWS_URL); 
     const newsList = [];
+    let template = `
+        <div>
+        <h1> Hacker News</h1>
+        <ul>
+        <li>
+        </li>
+        </ul>
+        <div>
+        <a href = "#">다음 페이지</a>
+        <a href = "#">이전 페이지</a>
+        </div>
+        </div>
+    `
 
     newsList.push('<ul>');
 
-for(let i =0; i <10; i++){
+    for(let i =(store.currentPage - 1) * 10; i < store.currentPage * 10; i++){
     newsList.push (`
     <li>
-        <a href = '#${newsFeed[i].id}'>
+        <a href = '#/show/${newsFeed[i].id}'>
         ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
-    </li>`)
-    
+    </li>`
+    );
 }
-
-newsList.push('<ul/>')
+newsList.push('<ul/>');
+newsList.push(`
+<div>
+ <a href = #/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}>이전 페이지 </a>
+ <a href = #/page/${store.currentPage + 1}>다음 페이지 </a>
+</div>
+`)
 //html 하단에 자식 노드로 추가
 container.innerHTML = newsList.join(''); //배열에 있는 문자열을 하나로 만든다 (join)
+}
 
 
+
+
+function newsDetail(){
+    //loaction 브라우저가 제공해주는 객체 ->주소와 관련된 다양한 정보를 알 수 있다
+const id = location.hash.substr(7);//#제거
+const newsCotent=getDate(CONTENT_URL.replace('@id',id))
+
+container.innerHTML=`
+    <h1> ${newsCotent.title} </h1>
+        <div>
+            <a href ="#/page/${store.currentPage}">목록으로</a>
+        </div>
+`;
+}
+function router(){
+    const routePath = location.hash;
+    if(routePath === ''){
+        newsFead();
+    }else if(routePath.indexOf('#/page/' >= 0)){
+        store.currentPage =Number(routePath.substr(7));
+        newsFead();
+    }else{
+        newsDetail();
+
+    }
+}
+
+window.addEventListener('hashchange',router)
+
+router();
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,6 +123,9 @@ var ajax = new XMLHttpRequest();
 var content = document.createElement('div');
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+var store = {
+  currentPage: 1
+};
 
 function getDate(url) {
   // ajax 함수
@@ -133,26 +136,46 @@ function getDate(url) {
   return JSON.parse(ajax.response); //json 을 객체로
 }
 
-var newsFeed = getDate(NEWS_URL);
-var ul = document.createElement('ul');
-window.addEventListener('hashchange', function () {
-  //loaction 브라우저가 제공해주는 객체 ->주소와 관련된 다양한 정보를 알 수 있다
-  var id = location.hash.substr(1); //#제거
+function newsFead() {
+  //글 목록 함수
+  var newsFeed = getDate(NEWS_URL);
+  var newsList = [];
+  var template = "\n        <div>\n        <h1> Hacker News</h1>\n        <ul>\n        <li>\n        </li>\n        </ul>\n        <div>\n        <a href = \"#\">\uB2E4\uC74C \uD398\uC774\uC9C0</a>\n        <a href = \"#\">\uC774\uC804 \uD398\uC774\uC9C0</a>\n        </div>\n        </div>\n    ";
+  newsList.push('<ul>');
 
-  var newsCotent = getDate(CONTENT_URL.replace('@id', id));
-  var title = document.createElement('h1');
-  container.innerHTML = "\n        <h1> ".concat(newsCotent.title, " <h1/>\n            <div>\n                <a href =\"#\">\uBAA9\uB85D\uC73C\uB85C<a/>\n            <div/>\n    ");
-});
-var newsList = [];
-newsList.push('<ul>');
+  for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+    newsList.push("\n    <li>\n        <a href = '#/show/".concat(newsFeed[i].id, "'>\n        ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n        </a>\n    </li>"));
+  }
 
-for (var i = 0; i < 10; i++) {
-  newsList.push("\n    <li>\n        <a href = '#".concat(newsFeed[i].id, "'>\n        ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n        </a>\n    </li>"));
+  newsList.push('<ul/>');
+  newsList.push("\n<div>\n <a href = #/page/".concat(store.currentPage > 1 ? store.currentPage - 1 : 1, ">\uC774\uC804 \uD398\uC774\uC9C0 </a>\n <a href = #/page/").concat(store.currentPage + 1, ">\uB2E4\uC74C \uD398\uC774\uC9C0 </a>\n</div>\n")); //html 하단에 자식 노드로 추가
+
+  container.innerHTML = newsList.join(''); //배열에 있는 문자열을 하나로 만든다 (join)
 }
 
-newsList.push('<ul/>'); //html 하단에 자식 노드로 추가
+function newsDetail() {
+  //loaction 브라우저가 제공해주는 객체 ->주소와 관련된 다양한 정보를 알 수 있다
+  var id = location.hash.substr(7); //#제거
 
-container.innerHTML = newsList.join(''); //배열에 있는 문자열을 하나로 만든다 (join)
+  var newsCotent = getDate(CONTENT_URL.replace('@id', id));
+  container.innerHTML = "\n    <h1> ".concat(newsCotent.title, " </h1>\n        <div>\n            <a href =\"#/page/").concat(store.currentPage, "\">\uBAA9\uB85D\uC73C\uB85C</a>\n        </div>\n");
+}
+
+function router() {
+  var routePath = location.hash;
+
+  if (routePath === '') {
+    newsFead();
+  } else if (routePath.indexOf('#/page/' >= 0)) {
+    store.currentPage = Number(routePath.substr(7));
+    newsFead();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+router();
 },{}],"../../../../../opt/homebrew/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
