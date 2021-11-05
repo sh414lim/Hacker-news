9c029fd3a3121bf55b61890751ea1eb4cdfb1269
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-11-05 10:09:01 +0900

    css

--- app.js
+++ app.js
@@ -90,12 +90,55 @@ const id = location.hash.substr(7);//#제거
 const newsContent=getDate(CONTENT_URL.replace('@id',id))
 
 container.innerHTML=`
-    <h1> ${newsContent.title} </h1>
-        <div>
-            <a href ="#/page/${store.currentPage}">목록으로</a>
-        </div>
+<div class="bg-gray-600 min-h-screen pb-8">
+<div class="bg-white text-xl">
+  <div class="mx-auto px-4">
+    <div class="flex justify-between items-center py-6">
+      <div class="flex justify-start">
+        <h1 class="font-extrabold">Hacker News</h1>
+      </div>
+      <div class="items-center justify-end">
+        <a href="#/page/${store.currentPage}" class="text-gray-500">
+          <i class="fa fa-times"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="h-full border rounded-xl bg-white m-6 p-4 ">
+  <h2>${newsContent.title}</h2>
+  <div class="text-gray-400 h-20">
+    ${newsContent.content}
+  </div>
+
+  {{__comments__}}
+
+</div>
+</div>
 `; 
+
+function makeComment(comments,called= 0){ //외부 인자를 받는 코멘트
+    const commentString = [];
+for(let i = 0; i < comments.length; i++){
+    commentString.push(`
+    <div style="padding-left: ${called * 40}px;" class="mt-4">
+          <div class="text-gray-400">
+            <i class="fa fa-sort-up mr-2"></i>
+            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+          </div>
+          <p class="text-gray-700">${comments[i].content}</p>
+        </div>    
+    `);
+    if(comments[i].comments,length > 0){
+      commentString.push(makeComment(comments[i].comments));//재귀호출
+    }
 }
+return commentString.join('');
+}
+container.innerHTML = template.replace('{{__comments__}}',makeComment(newsContent.comments));
+}
+
 function router(){
     const routePath = location.hash;
     if(routePath === ''){
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -174,7 +174,25 @@ function newsDetail() {
   var id = location.hash.substr(7); //#제거
 
   var newsContent = getDate(CONTENT_URL.replace('@id', id));
-  container.innerHTML = "\n    <h1> ".concat(newsContent.title, " </h1>\n        <div>\n            <a href =\"#/page/").concat(store.currentPage, "\">\uBAA9\uB85D\uC73C\uB85C</a>\n        </div>\n");
+  container.innerHTML = "\n<div class=\"bg-gray-600 min-h-screen pb-8\">\n<div class=\"bg-white text-xl\">\n  <div class=\"mx-auto px-4\">\n    <div class=\"flex justify-between items-center py-6\">\n      <div class=\"flex justify-start\">\n        <h1 class=\"font-extrabold\">Hacker News</h1>\n      </div>\n      <div class=\"items-center justify-end\">\n        <a href=\"#/page/".concat(store.currentPage, "\" class=\"text-gray-500\">\n          <i class=\"fa fa-times\"></i>\n        </a>\n      </div>\n    </div>\n  </div>\n</div>\n\n<div class=\"h-full border rounded-xl bg-white m-6 p-4 \">\n  <h2>").concat(newsContent.title, "</h2>\n  <div class=\"text-gray-400 h-20\">\n    ").concat(newsContent.content, "\n  </div>\n\n  {{__comments__}}\n\n</div>\n</div>\n");
+
+  function makeComment(comments) {
+    var called = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+    //외부 인자를 받는 코멘트
+    var commentString = [];
+
+    for (var i = 0; i < comments.length; i++) {
+      commentString.push("\n    <div style=\"padding-left: ".concat(called * 40, "px;\" class=\"mt-4\">\n          <div class=\"text-gray-400\">\n            <i class=\"fa fa-sort-up mr-2\"></i>\n            <strong>").concat(comments[i].user, "</strong> ").concat(comments[i].time_ago, "\n          </div>\n          <p class=\"text-gray-700\">").concat(comments[i].content, "</p>\n        </div>    \n    "));
+
+      if (comments[i].comments, length > 0) {
+        commentString.push(makeComment(comments[i].comments)); //재귀호출
+      }
+    }
+
+    return commentString.join('');
+  }
+
+  container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
 }
 
 function router() {
@@ -220,7 +238,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59291" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51630" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
